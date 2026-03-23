c216e5da9bbf91cc176f5caddf99f7a91ae7464b
Panwar, Ashish <ashishpanwar@microsoft.com>
2026-03-23 10:03:05 +0000

    increase torch symm mem allreduce buffer sizes
    Signed-off-by: Panwar, Ashish <ashishpanwar@microsoft.com>

--- python/sglang/srt/distributed/device_communicators/all_reduce_utils.py
+++ python/sglang/srt/distributed/device_communicators/all_reduce_utils.py
@@ -1,16 +1,18 @@
 MiB = 1024 * 1024
 
+GiB = 1024 * MiB
+
 TORCH_SYMM_MEM_ALL_REDUCE_MAX_SIZES = {
     9: {
-        2: 64 * MiB,  # 64 MB
-        4: 64 * MiB,  # 64 MB
-        6: 128 * MiB,  # 128 MB
-        8: 128 * MiB,  # 128 MB
+        2: 1 * GiB,
+        4: 1 * GiB,
+        6: 1 * GiB,
+        8: 1 * GiB,
     },
     10: {
-        2: 64 * MiB,  # 64 MB
-        4: 64 * MiB,  # 64 MB
-        6: 128 * MiB,  # 128 MB
-        8: 128 * MiB,  # 128 MB
+        2: 1 * GiB,
+        4: 1 * GiB,
+        6: 1 * GiB,
+        8: 1 * GiB,
     },
 }
